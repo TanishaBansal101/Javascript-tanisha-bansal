adbee354bb7bca51a128e0088a9763b9501248f3
TanishaBansal101 <145993687+TanishaBansal101@users.noreply.github.com>
2024-04-11 14:11:32 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId=220540
+let accountEmail="bansaltanisha101@gmail.com"
+var accountPassword="56723"
+accountCity="Jodhpur"
+let accountState;
+
+//accountId=2 //not allowed
+
+accountEmail="bansal101@gmail.com"
+accountPassword="248083"
+accountCity="Bharatpur"
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and function scope*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
