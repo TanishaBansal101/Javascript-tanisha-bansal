e6ae6f9803ec970d24707c959c5ae32dfdca6063
TanishaBansal101 <145993687+TanishaBansal101@users.noreply.github.com>
2024-04-11 14:41:32 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";    // treat all js code as newer version
+
+//alert(4+8)   // we are using nodejs,not browser
+console.log(4+8);
+console.log("Tanisha Bansal");
+
+let name ="Tanisha"
+let age="19"
+let isLoggedIn=false
+let state;
+
+//number=>2 to power 53
+//bigint
+//string=>" "
+//boolean=>true/false
+//null=>standardalone value
+//symbol=>unique
+//object
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);    //object
