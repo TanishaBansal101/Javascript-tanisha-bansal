ee556c081d7a60998afec52d786200ebc0321bfb
Tanisha <bansaltanisha101@gmail.com>
2024-06-26 15:07:31 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+/*let score ="33"
+console.log(typeof score);    => string
+console.log(typeof(score));    => string  */    
+
+/* let score = 33
+console.log(typeof score);     // =>number
+console.log(typeof(score));    // =>number     */
+
+/*let score = "33abc"         string
+                             string
+                             number
+                             NaN
+console.log(typeof score);     
+console.log(typeof(score));*/
+
+let score = null
+console.log(score);     
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/* for null  => 0
+for undefined  => NaN
+for boolean(true)  => 1  */
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=>false
+//"" =>false
+//"tanisha"=>true
+
+let number=33
+let stringNumber=String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
