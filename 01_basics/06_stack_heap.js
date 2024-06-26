74cec713079f054476249c30847b9837e065ae7d
Tanisha <bansaltanisha101@gmail.com>
2024-06-26 19:03:02 +0530

    stack and heap

---
+++ 01_basics/06_stack_heap.js
@@ -0,0 +1,20 @@
+// stack(Primitive), heap(Non-Primitive)
+
+let myYoutubename="bansaltanishadotcom"
+
+let anothername=myYoutubename
+anothername="Tanisha Bansal"
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne={
+    email:"user@google.com",
+    upi:"user@upi"
+}
+
+let userTwo= userOne
+
+userTwo.email="bansaltanisha101@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
