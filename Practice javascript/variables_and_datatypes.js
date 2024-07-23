bb7df62ea430111586ab871171f0670fdeb1c507
Tanisha Bansal <bansaltanisha101@gmail.com>
2024-07-23 07:50:38 +0530

    variables and datatypes

---
+++ Practice javascript/variables_and_datatypes.js
@@ -0,0 +1,36 @@
+// task 1
+var marks = 90;
+console.log(marks);
+console.log(typeof marks);
+
+// task 2
+let examMarks="95"
+console.log(examMarks);
+console.log(typeof examMarks);
+
+// task 3
+const isLoggedIn="1"
+const booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// task 4
+const arr=["tanisha","meet"]
+console.log(arr);
+console.log(typeof arr);
+
+const user={
+    firstName:"Tanisha",
+    lastName:"Bansal",
+    collegeName:"M.B.M University",
+    state:"Rajasthan"
+}
+console.log(user);
+console.log(user.state);
+console.log(typeof user);
+
+// task 5
+let city="Bharatpur"
+console.log(city);
+let CurrentCity=String(city)
+console.log(CurrentCity);
