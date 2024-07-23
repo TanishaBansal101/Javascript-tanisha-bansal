223af3528338c6f739372b85a1cfe37d976cd424
Tanisha Bansal <bansaltanisha101@gmail.com>
2024-07-23 07:50:11 +0530

    operators

---
+++ Practice javascript/operators.js
@@ -0,0 +1,31 @@
+// Arithmetic operators
+console.log(4+5);
+console.log(4-5);
+console.log(4*5);
+console.log(4/5);
+console.log(5%4);
+
+//Assignment Operators
+let x=7
+console.log(x+=5);
+
+let y=7
+console.log(y-=5);
+
+//comparision operators
+console.log(5>7);
+console.log(5<7);
+console.log(5>=5);
+console.log(5<=4);
+console.log(5=="5");
+console.log(5==="5");
+
+//logical operators
+console.log(5>2 && 4===4);
+console.log(5<2 || 4===4);
+console.log(5<-2 && 7>-8);
+
+//ternary operator
+const age=21;
+const status=age>=18?"Adult":"Minor";
+console.log(status);
