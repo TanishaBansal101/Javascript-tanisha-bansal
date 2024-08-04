07e7286b4a582cde71e9c1a797e96b2e17e958fe
Tanisha Bansal <bansaltanisha101@gmail.com>
2024-08-04 15:42:41 +0530

    function

---
+++ Practice javascript/function.js
@@ -0,0 +1,47 @@
+// task 1
+let n=4
+function evenOrOdd(n){
+    let result;
+    if(n%2==0){
+        result="Even";
+    }
+    else{
+        result="Odd";
+    }
+    return result;
+}
+console.log(evenOrOdd(n));
+
+//task 2
+let m=5;
+function calculateSquare(){
+    let result;
+    result=m*m;
+    return result;
+}
+console.log(calculateSquare(m));
+
+//task 3
+let num1=7,num2=4;
+function max(num1,num2){
+    let result;
+    if(num1>num2){
+        result="num1";
+    }
+    else{
+        result="num2";
+    }
+    return result;
+}
+console.log(max(num1,num2));
+
+// task 4
+let str1="Tanisha", str2=" Bansal";
+function concatenateString(){
+    let result;
+    result= str1+str2;
+    return result;
+}
+console.log(concatenateString(str1,str2));
+
+// task 5
