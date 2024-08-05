9a6464f6746efacaf90303881ad8a699e1e078a7
Tanisha Bansal <bansaltanisha101@gmail.com>
2024-08-05 14:25:53 +0530

    object

---
+++ Practice javascript/object.js
@@ -0,0 +1,12 @@
+//task 1
+const book={
+    title:"Life of Mahatma Gandhi",
+    author:"Mahatma Gandhi",
+    year:1900
+}
+
+// task 2
+console.log(book.title);
+console.log(book.author);
+
+// task 3
