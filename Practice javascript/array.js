b14c556c2b0c44a79b774f7f4b51cce13028638c
Tanisha Bansal <bansaltanisha101@gmail.com>
2024-08-05 14:25:34 +0530

    array

---
+++ Practice javascript/array.js
@@ -0,0 +1,33 @@
+// task 1
+let arr1 = [1, 2, 3, 4, 5];
+console.log(arr1);
+
+// task 2
+console.log(arr1[0]);
+console.log(arr1[4]);
+
+// task 3
+arr1.push(7)
+console.log(arr1);
+
+// task 4
+arr1.pop(7)
+console.log(arr1);
+
+// task 5
+arr1.shift(1)
+console.log(arr1);
+
+// task 6
+arr1.unshift(9)
+console.log(arr1);
+
+// task 7
+
+
+
+// task 10
+let arr2
+for(let i=1;i<=10;i++){
+    console.log(arr2);
+}
